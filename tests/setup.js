4cf5624e5cb76ad7b558ae5af967d6ab78975279
Martin Acosta <tinchoz49@gmail.com>
2019-10-07 13:17:15 -0300

    Added setup tests.

--- tests/setup.js
+++ tests/setup.js
@@ -1,6 +1,13 @@
+const waitFor = require('p-wait-for')
 const { run } = require('./helpers')
 
 module.exports = async () => {
-  console.log('\n\n*** Starting xd ***\n')
+  console.log('\n\n> Starting xd.')
   await run(process.cwd(), 'start')
+  await waitFor(async () => {
+    const { stdout } = await run(process.cwd(), 'status')
+    console.log(stdout)
+    return stdout.includes('Running.')
+  }, { interval: 1500, timeout: 5000 })
+  console.log('> Started xd.\n')
 }
--- tests/teardown.js
+++ tests/teardown.js
@@ -1,6 +1,13 @@
+const waitFor = require('p-wait-for')
 const { run } = require('./helpers')
 
 module.exports = async () => {
-  console.log('\n\n*** Closing xd ***\n')
+  console.log('\n\n>Closing xd.')
   await run(process.cwd(), 'stop')
+  await waitFor(async () => {
+    const { stdout } = await run(process.cwd(), 'status')
+    console.log(stdout)
+    return stdout.includes('Not running')
+  }, { interval: 1500, timeout: 5000 })
+  console.log('>Closed xd.\n')
 }
